81e91b3b82c182e78b69ecb6e7776ca6ce0519b3
Aleksandr Korshunov <eastoutcity@mail.ru>
2020-04-29 23:18:54 +0300

    Creating project

---
+++ src/test/java/com/kor/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kor.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
