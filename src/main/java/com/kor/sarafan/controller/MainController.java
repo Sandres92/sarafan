dad6a47fa3542b328f31b7c81d0814845799b991
Aleksandr Korshunov <eastoutcity@mail.ru>
2020-05-06 19:39:20 +0300

    add frontend tools

--- src/main/java/com/kor/sarafan/controller/MainController.java
+++ src/main/java/com/kor/sarafan/controller/MainController.java
@@ -3,6 +3,7 @@ package com.kor.sarafan.controller;
 import com.kor.sarafan.domain.User;
 import com.kor.sarafan.repo.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,9 @@ public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
+    @Value("${spring.profile.active}")
+    private String profile;
+
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal User user) {
         HashMap<Object, Object> data = new HashMap<>();
@@ -25,6 +29,8 @@ public class MainController {
         data.put("messages", messageRepo.findAll());
 
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
+
         return "index";
     }
 }
---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-if="!profile">log in through
+            <a href="/login">google</a>
+        </div>
+        <div v-else>
+            <div>{{profile.name}}&nbsp;<a href="/logout">log out</a></div>
+            <messages-list :messages="messages"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MessagesList from 'components/messages/MessageList.vue'
+
+    export default {
+        components: {
+            MessagesList
+        },
+        data() {
+            return {
+                messages: frontendData.messages,
+                profile: frontendData.profile
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
